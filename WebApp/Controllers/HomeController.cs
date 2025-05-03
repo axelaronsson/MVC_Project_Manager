14e89e74d17a910aa409caf29a2d64a43c14ea98
Axel <Guitaron@TVST-GTN45>
2025-05-04 00:32:56 +0200

    add markup for project rendering

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -11,7 +11,82 @@ namespace WebApp.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var project = new ProjectModel
+            {
+                Id = 1,
+                Name = "Website Redesign",
+                Company = "GitLab Inc.",
+                Description = "It is necessary to develop a website redesign in a corporate style."
+            };
+
+            var project2 = new ProjectModel
+            {
+                Id = 2,
+                Name = "Landing Page",
+                Company = "Bitbucket, Inc.",
+                Description = "It is necessary to create a landing together with the development of design."
+            };
+
+            var project3 = new ProjectModel
+            {
+                Id = 3,
+                Name = "Parser Development",
+                Company = "Driveway, Inc.",
+                Description = "Create a mobile application on iOS and Android devices."
+            };
+
+            var project4 = new ProjectModel
+            {
+                Id = 4,
+                Name = "Parser Development",
+                Company = "Driveway, Inc.",
+                Description = "Create a mobile application on iOS and Android devices."
+            };
+
+            var project5 = new ProjectModel
+            {
+                Id = 5,
+                Name = "Parser Development",
+                Company = "Driveway, Inc.",
+                Description = "Create a mobile application on iOS and Android devices."
+            };
+
+            var project6 = new ProjectModel
+            {
+                Id = 6,
+                Name = "Parser Development",
+                Company = "Driveway, Inc.",
+                Description = "Create a mobile application on iOS and Android devices."
+            };
+
+            var project7 = new ProjectModel
+            {
+                Id = 7,
+                Name = "Parser Development",
+                Company = "Driveway, Inc.",
+                Description = "Create a mobile application on iOS and Android devices."
+            };
+
+            var project8 = new ProjectModel
+            {
+                Id = 8,
+                Name = "Parser Development",
+                Company = "Driveway, Inc.",
+                Description = "Create a mobile application on iOS and Android devices."
+            };
+
+            var list = new List<ProjectModel> {
+                project,
+                project2,
+                project3,
+                project4,
+                //project5,
+                //project6,
+                //project7,
+                //project8,
+            };
+
+            return View(list);
         }
 
         public IActionResult Privacy()
